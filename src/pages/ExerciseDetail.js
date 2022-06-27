3905441c19ec9216e4ff7ddf5d581eb2b12f69dd
Elizabeth Popova <elizabethpopova2020@gmail.com>
2022-06-27 02:03:58 +0200

    initializing commit

---
+++ src/pages/ExerciseDetail.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
+
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
+
+
+const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams(); 
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+
+    }
+    
+    fetchExercisesData();
+  }, [id])
+
+  return (
+    <Box sx={{ mt: { lg: '96px', xs: '60px' } }}>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos  />
+      <SimilarExercises  />
+    </Box>
+  );
+}
+
+export default ExerciseDetail;
