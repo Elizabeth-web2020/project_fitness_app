7d05edcfa34eece5e527a12811e5d8dcf9c03fab
Elizabeth Popova <elizabethpopova2020@gmail.com>
2022-06-28 03:03:33 +0200

    were fixed all errors and refactoring files

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -40,11 +40,13 @@ const ExerciseDetail = () => {
     fetchExercisesData();
   }, [id])
 
+  if (!exerciseDetail) return <div>No Data</div>;
+
   return (
     <Box sx={{ mt: { lg: '96px', xs: '60px' } }}>
       <Detail exerciseDetail={exerciseDetail} />
       <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
-      <SimilarExercises  />
+      <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises} />
     </Box>
   );
 }
