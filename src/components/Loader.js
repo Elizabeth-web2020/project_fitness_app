bfc876331a8f9eb66b93d027dee297249cc49705
Elizabeth Popova <elizabethpopova2020@gmail.com>
2022-06-28 02:56:22 +0200

    added loading at the page

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import { Stack } from '@mui/material';
+import { TailSpin } from 'react-loader-spinner';
 
-const Loader = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-}
+const Loader = () => (
+  <Stack direction="row" justifyContent="center" alignItems="center" width="100%">
+    <TailSpin color="grey" />
+  </Stack>
+);
 
 export default Loader;
